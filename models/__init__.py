0f3b87ccaf78150561bde8845ca324c9fdcd75f2
anonyauth <123731656+anonyauth@users.noreply.github.com>
2024-02-01 11:47:07 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from .odnn import ODNN
---
+++ models/odnn.py
@@ -0,0 +1,25 @@
+import numpy as np
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+from utils import layers
+from models.base_rnnets import BaseODNN
+
+class ODNN(BaseODNN):
+    def inference(inputs, nb_classes, training, ffd_drop,
+            norm_mat, output_dim, activation=tf.nn.elu):
+        
+        h_1 = layers.DeGroot(inputs, norm_mat=norm_mat,
+            output_dim=output_dim, activation=activation,
+            in_drop=ffd_drop)
+
+        # h_1 = layers.Friedkin_Johnsen(inputs, norm_mat=norm_mat,
+        #     output_dim=output_dim, activation=activation,
+        #     in_drop=ffd_drop)
+        
+        logits = layers.linear_layer(h_1,
+            output_dim=nb_classes, activation=lambda x: x,
+            in_drop=ffd_drop)
+    
+        return logits
